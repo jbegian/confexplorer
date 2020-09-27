873579d99a43755011deccae9a56f4331949c5ee
Jordan Begian <begian.j@gmail.com>
2020-09-27 16:42:13 -0400

    add fetch service to fetch list of videos

---
+++ src/main/kotlin/Fetch.kt
@@ -0,0 +1,17 @@
+import kotlinx.browser.window
+import kotlinx.coroutines.*
+
+suspend fun fetchVideo(id: Int): Video =
+        window.fetch("https://my-json-server.typicode.com/kotlin-hands-on/kotlinconf-json/videos/$id")
+                .await()
+                .json()
+                .await()
+                .unsafeCast<Video>()
+
+suspend fun fetchVideos(): List<Video> = coroutineScope {
+    (1..25).map { id ->
+        async {
+            fetchVideo(id)
+        }
+    }.awaitAll()
+}
